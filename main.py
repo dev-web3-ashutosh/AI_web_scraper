f90d35ba4b758d9d19155dd3f0d34b97a3eda08e
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2025-03-11 23:28:01 +0530

    added code in main.py to scrape the input url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from scrape import scrape_website
 
 # main heading
 st.title("AI Web Scraper")
@@ -8,5 +9,6 @@ url = st.text_input("Enter a Website URL: ")
 # when the button "Scrape Site" is clicked
 if st.button("Scrape Site!"):
     st.write("Scraping the website...")
-
+    result = scrape_website(url)
+    print(result)
 
