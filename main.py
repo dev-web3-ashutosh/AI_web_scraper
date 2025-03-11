c43e8c2e71215f4a131f48c388882108ddba693e
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2025-03-11 21:58:50 +0530

    Streamlit code to create frontend

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("AI Web Scraper")
+# insert a text input box
+url = st.text_input("Enter a Website URL: ")
+
+# when the button "Scrape Site" is clicked
+if st.button("Scrape Site!"):
+    st.write("Scraping the website...")
+
+
