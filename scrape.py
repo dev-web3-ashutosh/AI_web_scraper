d5d9eaba1764e2c9fbe78dde4eade9978b7cdccf
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2025-03-11 23:13:41 +0530

    import time

--- scrape.py
+++ scrape.py
@@ -1,3 +1,21 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
+def scrape_website(website):
+    # to check if func is working
+    print("Launching Chrome Browser...")
+
+    chrome_driver_path=""
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print("Page Loaded...")
+        html = driver.page_source
+        time.sleep(10)
+        
+        return html
+    finally:
+        driver.quit()
