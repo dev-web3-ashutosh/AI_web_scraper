d08b87d4142019d3950a1a16554c17ecec3df39e
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2025-03-13 03:00:22 +0530

    add function to clean content

--- scrape.py
+++ scrape.py
@@ -27,3 +27,17 @@ def extract_body_content(html_content):
     if body_content:
         return str(body_content)
     return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(seperator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
