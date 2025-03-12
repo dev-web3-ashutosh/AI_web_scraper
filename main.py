124ff20cb2a7c23f0e7a66167af8c200255b67ab
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2025-03-13 03:21:25 +0530

    load and view content using streamlit

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ if st.button("Scrape Site!"):
     # clean the content to remove content in script and style tags
     cleaned_content = clean_body_content(body_content)
 
+    # store content in streamlit session
+    st.session_state.dom_content = cleaned_content
+
+    # show content in collapsible streamlit expander
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
     
 
     print(result)
